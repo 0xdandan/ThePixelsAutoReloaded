cccef2c11467ecea4b36b85e802f65aa889656ed
0xdandan <85060199+0xdandan@users.noreply.github.com>
2024-03-22 12:23:15 +0700

    Update TPAR_main.js

--- TPAR_main.js
+++ TPAR_main.js
@@ -1,2 +1,2 @@
 // This is the main script you should execute, the link below will be updated when a new version of the script is released so you don't have to change it manually.
-fetch('https://raw.githubusercontent.com/0xdandan/ThePixelsAuto/main/TPAR_v4.x.js').then(response => response.text()).then(script => eval(script));
+fetch('https://raw.githubusercontent.com/0xdandan/ThePixelsAutoReloaded/main/TPAR_v4.x.js').then(response => response.text()).then(script => eval(script));
